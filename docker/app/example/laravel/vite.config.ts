aca6d0aa1631e3d4dfbd942132ea3b1691eba35f
Mojtaba <efmojtaba1@gmail.com>
2026-08-13 00:23:49 +0330

    Fresh example template fo laravel

--- docker/app/example/laravel/vite.config.ts
+++ docker/app/example/laravel/vite.config.ts
@@ -3,22 +3,19 @@ import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
+import { bunny } from 'laravel-vite-plugin/fonts';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            host: 'localhost',
-        },
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
             refresh: true,
-            fonts: false, // غیرفعال کردن دریافت خودکار فونت‌های راه دور
+            fonts: [
+                bunny('Instrument Sans', {
+                    weights: [400, 500, 600],
+                }),
+            ],
         }),
         inertia(),
         react({
